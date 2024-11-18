6f60adba08b3b6d13915cf7896251c501d96b117
Nathan Pietrantonio <pietrann@uci.edu>
2024-11-17 19:50:38 -0800

    Change to JSX for App

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
