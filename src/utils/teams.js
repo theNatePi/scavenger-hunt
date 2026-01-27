ccaf8f522bb058f2edc9527894eaee00771929b7
Nathan Pietrantonio <pietrann@uci.edu>
2026-01-27 15:24:06 -0800

    Add team creation logic

---
+++ src/utils/teams.js
@@ -0,0 +1,43 @@
+
+function _shuffle(array) {
+  // Use Fisher-Yates shuffle
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+
+export function createTeams(playerIds, teamSize=undefined, numTeams=undefined) {
+  if (teamSize && numTeams) {
+    throw new Error('Cannot specify both teamSize and numTeams');
+  }
+  if (teamSize === undefined && numTeams === undefined) {
+    throw new Error('Must specify either teamSize or numTeams');
+  }
+
+  if (numTeams === undefined) {
+    // round to nearest integer
+    numTeams = Math.round(playerIds.length / teamSize);
+  }
+
+  // Shuffle playerIds
+  playerIds = _shuffle(playerIds);
+
+  const teams = [];
+  let teamIndex = 0;
+  for (const playerId of playerIds) {
+    if (teamIndex === numTeams) {
+      teamIndex = 0;
+    }
+    if (teams[teamIndex] === undefined) {
+      teams[teamIndex] = [];
+    }
+
+    teams[teamIndex].push(playerId);
+    teamIndex++;
+  }
+  console.log(teams);
+  return teams;
+}
