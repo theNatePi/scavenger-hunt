0513a77a8d87113771fcae779dc6b1d3d5a79ae7
Nathan Pietrantonio <pietrann@uci.edu>
2026-01-30 16:45:53 -0800

    Enable caching

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { GameProvider } from './contexts/GameContext';
 import Playground from './pages/playground';
 import Landing from './pages/landing/landing';
@@ -10,28 +11,32 @@ import Game from './pages/Game/game';
 import GameItem from './pages/Game/GameItem';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <Router>
-      <GameProvider>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/admin/createGame" element={<CreateGame />} />
-          <Route path="/lobby" element={<Lobby />} />
-          <Route path="/lobby/teamReveal" element={<TeamReveal />} />
-          <Route path="/game" element={<Game />} />
-          <Route path="/game/item/:id" element={<GameItem />} />
-          {/* <Route path="/joinLobby/teamReveal" element={<></>} />
-          <Route path="/game" element={<></>} />
-          <Route path="/game/item/:id" element={<></>} />
-          <Route path="/game/gameOver" element={<></>} />
-          <Route path="/game/winnerReveal" element={<></>} />
+      <QueryClientProvider client={queryClient}>
+        <GameProvider>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/admin/createGame" element={<CreateGame />} />
+            <Route path="/lobby" element={<Lobby />} />
+            <Route path="/lobby/teamReveal" element={<TeamReveal />} />
+            <Route path="/game" element={<Game />} />
+            <Route path="/game/item/:id" element={<GameItem />} />
+            {/* <Route path="/joinLobby/teamReveal" element={<></>} />
+            <Route path="/game" element={<></>} />
+            <Route path="/game/item/:id" element={<></>} />
+            <Route path="/game/gameOver" element={<></>} />
+            <Route path="/game/winnerReveal" element={<></>} />
 
-        
-          <Route path="/admin/runGame" element={<></>} />
-          <Route path="/admin/runGame/team/:id" element={<></>} />
-          <Route path="/admin/runGame/team/:id/item/:id" element={<></>} /> */}
-        </Routes>
-      </GameProvider>
+          
+            <Route path="/admin/runGame" element={<></>} />
+            <Route path="/admin/runGame/team/:id" element={<></>} />
+            <Route path="/admin/runGame/team/:id/item/:id" element={<></>} /> */}
+          </Routes>
+        </GameProvider>
+      </QueryClientProvider>
     </Router>
   );
 }
