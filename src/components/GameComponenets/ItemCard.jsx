e0aa90909c511b5b8a6ddef5589a99661bff5531
Nathan Pietrantonio <pietrann@uci.edu>
2026-02-04 11:14:42 -0800

    Improve image loading

--- src/components/GameComponenets/ItemCard.jsx
+++ src/components/GameComponenets/ItemCard.jsx
@@ -4,7 +4,7 @@ import GlassButton from '../glassButton';
 import ClickableImage from './ClickableImage';
 
 export default function ItemCard({ itemId, itemImgUri, points, bonusPoints, teamsFound, isFound, foundImgUri}) {
-  const [showFoundImage, setShowFoundImage] = useState(false);
+  const [showFoundImage, setShowFoundImage] = useState(foundImgUri ? true : false);
   const cardColor = isFound ? 'var(--confirm-color-transparent)' : 'var(--not-found-color)';
   const cardGradient = isFound ? 'var(--found-gradient)' : 'var(--not-found-gradient)';
 
@@ -27,17 +27,37 @@ export default function ItemCard({ itemId, itemImgUri, points, bonusPoints, team
       >
         <p>{isFound ? 'Found' : 'Not Found'}</p>
       </GlassContainer>
-      <ClickableImage
-        imgUri={showFoundImage ? foundImgUri : itemImgUri}
-        alt={itemId}
+      <div
         style={{
-          width: '100%',
-          height: '100%',
-          objectFit: 'cover',
-          objectPosition: 'center',
-          borderRadius: '20px',
+          minHeight: '50px',
         }}
-      />
+      >
+        {showFoundImage ? (
+          <ClickableImage
+            imgUri={foundImgUri}
+            alt={itemId}
+            style={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+              objectPosition: 'center',
+              borderRadius: '20px',
+            }}
+          />
+        ) : (
+          <ClickableImage
+            imgUri={itemImgUri}
+            alt={itemId}
+            style={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+              objectPosition: 'center',
+              borderRadius: '20px',
+            }}
+          />
+        )}
+      </div>
       {isFound && (
         <GlassButton onClick={() => setShowFoundImage(!showFoundImage)}>
           {showFoundImage ? 'Show Original' : 'Show Found'}
